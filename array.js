ef2db0f329738109e4c3f074663124fb91695716
Ashwini Jadhav <ashwiniphule17@gmail.com>
2023-07-11 07:52:22 +0530

    basic files
    basic files

--- array.js
+++
@@ -1,7 +0,0 @@
-// homogenous array
-var courses = ['Angular', 'NodeJs', 'JavaScript'];
-console.log(courses[0]);
-//courses.push(10);
-// hetro 
-var cousr2 = ['Angular', 123];
-console.log(cousr2);
